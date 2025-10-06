5036ace8641d3c404e272cea426e496f09515bbe
Kenley Bides <kenbides1@gmail.com>
2025-10-06 10:50:30 -0400

    inputs created

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,4 +1,5 @@
 // Lab 3
+'use client'
 import { useForm } from "react-hook-form";
 
 interface PostFormData {
@@ -11,22 +12,33 @@ export default function Register() {
 
  const { register, handleSubmit, formState: { errors, isSubmitSuccessful }} = useForm<PostFormData>();
 
+ const onSubmit = (data: PostFormData) => {
+    console.log("Success:", data);
+ }
     return (
         <main>
             <h1> Register </h1>
-            <fieldset>
+            <form>
+            <fieldset onSubmit={handleSubmit(onSubmit)}>
                 <label>Username</label>
-                <input type="username"></input>
+                <input type="text"{...register("username", {required: "Username is required"})}/>
+                {errors.username && <p>{errors.username.message}</p>}
             </fieldset>
+
             <fieldset>
                 <label>Password</label>
-                <input type="password"></input>
+                <input type="password"{...register("password", {required: "Password is required"})}/>
+                {errors.password && <p>{errors.password.message}</p>}
             </fieldset>
+
             <fieldset>
                 <label>Confirm</label>
-                <input type="password"></input>
+                <input type="password"{...register("password", {required: "Password is required"})}/>
+                {errors.password && <p>{errors.password.message}</p>}
             </fieldset>
+
             <button>Register</button>
+            </form>
         </main>
     )
 }
